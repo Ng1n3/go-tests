c96fb4bc7f619b6541f025b3e9af44d37095505f
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-25 20:51:21 +0100

    feat: question 6

---
+++ exercise6/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+  
+}
+
+
+/*
+in addition to the main goroutine, launch two additional goroutines
+○ each additional goroutine should print something out
+● use waitgroups to make sure each goroutine finishes before your program exists
+*/
