a0b9e296af17cefbddb3d852b9b80f5a2c23102c
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-25 21:24:30 +0100

    feat: answer 6

--- exercise6/main.go
+++ exercise6/main.go
@@ -1,9 +1,36 @@
 package main
 
-func main() {
-  
-}
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
+func main() {
+	fmt.Println("begin cpu", runtime.NumCPU())
+	fmt.Println("begin gs", runtime.NumGoroutine())
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() {
+		fmt.Println("first go routine")
+		wg.Done()
+	}()
+	go func() {
+		fmt.Println("first go routine 2")
+		wg.Done()
+	}()
+
+  fmt.Println("mid cpu", runtime.NumCPU())
+	fmt.Println("mid gs", runtime.NumGoroutine())
+
+	wg.Wait()
+
+	fmt.Println("about to exit")
+	fmt.Println("end cpu", runtime.NumCPU())
+	fmt.Println("end gs", runtime.NumGoroutine())
+}
 
 /*
 in addition to the main goroutine, launch two additional goroutines
