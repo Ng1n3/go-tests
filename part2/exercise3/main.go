78923abf3112f156e789c46d5db24d30f49bf7b6
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-28 13:52:11 +0100

    Feat: question & answer part2 exercise3

--- part2/exercise3/main.go
+++ part2/exercise3/main.go
@@ -11,16 +11,25 @@ func main() {
 	fmt.Println("about to exit")
 }
 
+func receive(c <-chan int) {
+	for v := range c {
+		fmt.Println(v)
+	}
+}
+
 func gen() <-chan int {
 	c := make(chan int)
 
-	for i := 0; i < 100; i++ {
-		c <- i
-	}
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+
+	}()
 
 	return c
 }
-
 
 /*
 Starting with this code, pull the values off the channel using a for range loop
