10afc7bb416204be7a84fd7819e61e89080419e2
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-28 13:04:20 +0100

    Feat: question & answer part2 exercise2

---
+++ part2/exercise2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cs := make(chan int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
+}
+
+/*
+Get the code up and running:
+*/
